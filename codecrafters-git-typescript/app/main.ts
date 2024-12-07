a4885329e1fb63a32e08c5f0e3b1db5213516bce
Leo Kovačević <leo.kowacevic@gmail.com>
2024-12-07 22:41:40 +0100

    cat file -p

--- codecrafters-git-typescript/app/main.ts
+++ codecrafters-git-typescript/app/main.ts
@@ -1,13 +1,16 @@
 import * as fs from "fs";
+import zlib from "zlib";
 
 const args = process.argv.slice(2);
 const command = args[0];
 
 enum Commands {
   Init = "init",
+  catFile = "cat-file",
 }
 
 switch (command) {
+  // INIT
   case Commands.Init:
     fs.mkdirSync(".git", { recursive: true });
     fs.mkdirSync(".git/objects", { recursive: true });
@@ -15,6 +18,26 @@ switch (command) {
     fs.writeFileSync(".git/HEAD", "ref: refs/heads/main\n");
     console.log("Initialized git directory");
     break;
+
+  // CAT FILE
+  case Commands.catFile:
+    const flag = args[1];
+    const shaHash = args[2];
+
+    if (flag === "-p") {
+      const blobDir = shaHash.slice(0, 2);
+      const blobFile = shaHash.slice(2);
+
+      const blob = fs.readFileSync(`.git/objects/${blobDir}/${blobFile}`);
+      const decompressedBlob = zlib.unzipSync(new Uint8Array(blob));
+      const nullByteIndex = decompressedBlob.indexOf(0);
+
+      process.stdout.write(
+        decompressedBlob.slice(nullByteIndex + 1).toString()
+      );
+    }
+    break;
+
   default:
     throw new Error(`Unknown command ${command}`);
 }
