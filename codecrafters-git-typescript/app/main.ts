7a8408cb92b92dde431e1fae4654e639146a5db2
Leo Kovačević <leo.kowacevic@gmail.com>
2024-12-10 22:19:15 +0100

    read a tree

--- codecrafters-git-typescript/app/main.ts
+++ codecrafters-git-typescript/app/main.ts
@@ -1,6 +1,7 @@
 import * as fs from "fs";
 import zlib from "zlib";
 import crypto from "crypto";
+import type { Entry } from "./model";
 
 const args = process.argv.slice(2);
 const command = args[0];
@@ -10,7 +11,9 @@ enum Commands {
   Init = "init",
   catFile = "cat-file",
   hashObject = "hash-object",
+  lsTree = "ls-tree",
 }
+const entries: Entry[] = [];
 
 switch (command) {
   // INIT
@@ -30,12 +33,16 @@ switch (command) {
       const blobDir = shaHash.slice(0, 2);
       const blobFile = shaHash.slice(2);
 
-      const blob = fs.readFileSync(`.git/objects/${blobDir}/${blobFile}`);
-      const decompressedBlob = zlib.unzipSync(new Uint8Array(blob));
-      const nullByteIndex = decompressedBlob.indexOf(0);
+      const blobContent = fs.readFileSync(
+        `.git/objects/${blobDir}/${blobFile}`
+      );
+      const decompressedBlobContent = zlib.unzipSync(
+        new Uint8Array(blobContent)
+      );
+      const nullByteIndex = decompressedBlobContent.indexOf(0);
 
       process.stdout.write(
-        decompressedBlob.slice(nullByteIndex + 1).toString()
+        decompressedBlobContent.slice(nullByteIndex + 1).toString()
       );
     }
     break;
@@ -73,6 +80,52 @@ switch (command) {
 
     break;
 
+  // LS TREE
+  case Commands.lsTree:
+    if (flag === "--name-only") {
+      const treeSha1Hash = args[2];
+
+      const treeDir = treeSha1Hash.slice(0, 2);
+      const treeFile = treeSha1Hash.slice(2);
+
+      const treeContent = fs.readFileSync(
+        `.git/objects/${treeDir}/${treeFile}`
+      );
+
+      const decompressedTreeContent = zlib.unzipSync(
+        new Uint8Array(treeContent)
+      );
+
+      parseTreeEntries(
+        decompressedTreeContent.slice(decompressedTreeContent.indexOf(0) + 1)
+      );
+      entries.forEach((entry) => console.log(entry.name));
+    }
+    break;
+
   default:
-  // throw new Error(`Unknown command ${command}`);
+    throw new Error(`Unknown command ${command}`);
+}
+
+function parseTreeEntries(buf: Buffer) {
+  if (!buf.length) {
+    return;
+  }
+
+  const spaceCharIndex = buf.indexOf(32);
+  const nullByteIndex = buf.indexOf(0);
+
+  const mode = buf.slice(0, spaceCharIndex).toString();
+  const name = buf.slice(spaceCharIndex + 1, nullByteIndex).toString();
+  const sha1Hash = buf.slice(nullByteIndex + 1, nullByteIndex + 1 + 20);
+
+  const entry: Entry = {
+    name,
+    mode,
+    sha1Hash,
+  };
+  entries.push(entry);
+  const nextOffset = mode.length + 1 + name.length + 1 + sha1Hash.length + 1;
+
+  parseTreeEntries(buf.slice(nextOffset));
 }
---
+++ codecrafters-git-typescript/app/model.ts
@@ -0,0 +1,5 @@
+export interface Entry {
+  name: string;
+  mode: string;
+  sha1Hash: Buffer;
+}
