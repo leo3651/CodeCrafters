2b8121ab668152919364f7e79867c1acad6d6576
Leo Kovačević <leo.kowacevic@gmail.com>
2025-02-18 20:38:53 +0100

    autocompletion

--- codecrafters-shell-typescript/app/main.ts
+++ codecrafters-shell-typescript/app/main.ts
@@ -5,6 +5,11 @@ import {
   handleRedirectCommand,
   isRedirectCommand,
 } from "./commandHandler";
+import fs from "fs";
+
+const AUDIO_CODE = "\u0007";
+const MOVE_TO_LINE_END = { ctrl: true, name: "e" };
+const CLEAR_CURRENT_LINE = { ctrl: true, name: "u" };
 
 const rl = createInterface({
   input: process.stdin,
@@ -41,3 +46,51 @@ function question(): void {
     question();
   });
 }
+
+process.stdin.on("keypress", (_, key) => {
+  if (key.sequence === "\t") {
+    const completions = ["echo", "exit"];
+
+    const envPaths = process.env.PATH?.split(":");
+    const eligiblePaths: string[] =
+      envPaths
+        ?.flatMap((path) => {
+          try {
+            return fs
+              .readdirSync(path)
+              .filter((fileName) =>
+                fileName.startsWith(rl.line.replaceAll("\t", ""))
+              );
+          } catch (err) {
+            return [];
+          }
+        })
+        .filter((file) => file !== undefined && file !== null) || [];
+
+    let hits = completions.filter((c) =>
+      c.startsWith(rl.line.replaceAll("\t", ""))
+    );
+
+    hits = [...new Set([...hits, ...eligiblePaths])];
+    // console.log("hits", hits);
+    // console.log("line", [rl.line]);
+
+    if (hits.length === 1) {
+      // console.log("ONE HIT");
+      rl.write(null, CLEAR_CURRENT_LINE);
+      rl.write(hits[0] + " ");
+    } else if (hits.length > 1) {
+      // console.log("HITSSS");
+      process.stdout.write(`\n${completions.join(" ")}\n`);
+      process.stdout.write("\r$ " + rl.line.replaceAll("\t", ""));
+    } else {
+      // console.log("AUDIO");
+      process.stdout.write("\r$ " + rl.line.trim() + AUDIO_CODE);
+    }
+  }
+
+  if (key.ctrl && key.name === "c") {
+    console.log("Exiting...");
+    process.exit();
+  }
+});
