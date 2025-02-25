9a91cce7a392c6e393b1393d13071eab861a52ed
Leo Kovačević <leo.kowacevic@gmail.com>
2025-02-25 21:25:11 +0100

    char classes combine

--- codecrafters-grep-typescript/app/main.ts
+++ codecrafters-grep-typescript/app/main.ts
@@ -3,7 +3,7 @@ const pattern = args[3];
 
 const inputLine: string = await Bun.stdin.text();
 console.log(`Input Line: ${inputLine}`);
-console.log(`Pattern: ${pattern}`);
+console.log(`Pattern: ${pattern}, len: ${pattern.length}`);
 
 if (args[2] !== "-E") {
   console.log("Expected first argument to be '-E'");
@@ -19,9 +19,96 @@ if (matchPattern(inputLine, pattern)) {
 }
 
 function matchPattern(inputLine: string, pattern: string): boolean {
+  // Match literal char
   if (pattern.length === 1) {
-    return inputLine.includes(pattern);
-  } else {
-    throw new Error(`Unhandled pattern: ${pattern}`);
+    return matchLiteralChar(pattern, inputLine);
   }
+
+  // Includes any digit
+  else if (pattern === "\\d") {
+    return matchAnyDigit(inputLine);
+  }
+
+  // Match alphanumeric characters
+  else if (pattern === "\\w") {
+    return matchAlphanumericChars(inputLine);
+  }
+
+  // Match negative characters group
+  else if (pattern.startsWith("[^") && pattern.endsWith("]")) {
+    const charsArr = pattern.slice(2, -1).split("");
+    return charsArr.every((char) => !inputLine.includes(char));
+  }
+
+  // Match positive characters group
+  else if (pattern.startsWith("[") && pattern.endsWith("]")) {
+    const charsArr = pattern.slice(1, -1).split("");
+    return charsArr.some((char) => inputLine.includes(char));
+  }
+
+  // Unhandled pattern
+  else {
+    return combiningCharClasses(inputLine, pattern);
+  }
+}
+
+function matchLiteralChar(char: string, inputLine: string): boolean {
+  return inputLine.includes(char);
+}
+
+function matchAnyDigit(inputLine: string): boolean {
+  for (let i = 0; i < inputLine.length; i++) {
+    if (inputLine[i].charCodeAt(0) >= 48 && inputLine[i].charCodeAt(0) <= 57) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function matchAlphanumericChars(inputLine: string) {
+  for (let i = 0; i < inputLine.length; i++) {
+    if (
+      (inputLine[i].charCodeAt(0) >= 48 && inputLine[i].charCodeAt(0) <= 57) ||
+      (inputLine[i].charCodeAt(0) >= 65 && inputLine[i].charCodeAt(0) <= 90) ||
+      (inputLine[i].charCodeAt(0) >= 97 && inputLine[i].charCodeAt(0) <= 122) ||
+      inputLine[i].charCodeAt(0) === 95
+    ) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function combiningCharClasses(inputLine: string, pattern: string) {
+  for (let i = 0; i < inputLine.length; i++) {
+    for (
+      let patternIndex = 0, inputLineIndex = 0;
+      patternIndex < pattern.length;
+      patternIndex++, inputLineIndex++
+    ) {
+      if (pattern[patternIndex] === "\\") {
+        patternIndex++;
+
+        if (
+          pattern[patternIndex] === "d" &&
+          !matchAnyDigit(inputLine[i + inputLineIndex])
+        ) {
+          break;
+        } else if (
+          pattern[patternIndex] === "w" &&
+          !matchAlphanumericChars(inputLine[i + inputLineIndex])
+        ) {
+          break;
+        }
+      } else if (inputLine[i + inputLineIndex] !== pattern[patternIndex]) {
+        break;
+      }
+
+      if (patternIndex === pattern.length - 1) {
+        return true;
+      }
+    }
+  }
+
+  return false;
 }
