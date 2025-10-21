696a14d5ecad7464a83a64f3796e54eeecf693b3
Leo Kovačević <leo.kowacevic@gmail.com>
2025-10-21 23:09:13 +0200

    handle files

--- codecrafters-grep-typescript/app/main.ts
+++ codecrafters-grep-typescript/app/main.ts
@@ -1,6 +1,7 @@
 import { MatchAST } from "./matchAST";
-import { RegexASTHandler } from "./regexAST";
+import { RegexASTHandler, type RegexAST } from "./regexAST";
 import { Tokenizer } from "./tokenizer";
+import fs from "fs";
 
 /*
 matchPattern("banana", "[^anb]");
@@ -16,50 +17,101 @@ matchPattern(
 );
 */
 
-const args = process.argv;
-const pattern = args[3];
+const args: string[] = process.argv;
+const pattern: string = args[3];
+const filePaths: string[] = args.slice(4);
 const inputLine: string = await Bun.stdin.text();
 
-console.log(`Input Line: ${inputLine}`);
-console.log(`Pattern: ${pattern}, len: ${pattern.length}`);
-console.log("\n");
+// console.log(args);
+// console.log(`Input Line: ${inputLine}`);
+// console.log(`Pattern: ${pattern}, len: ${pattern.length}`);
+// console.log("\n");
 
 if (args[2] !== "-E" && args[2] !== "-r") {
   console.log("Expected first argument to be '-E' or '-r'");
   process.exit(1);
 }
 
-if (matchPattern(inputLine, pattern)) {
-  console.log("Character is found. Exiting with 0...");
-  process.exit(0);
+if (filePaths.length) {
+  const matchedLines: string[][] = getMatchedLinesFromFiles(filePaths, pattern);
+
+  if (matchedLines.length) {
+    matchedLines.forEach((file) => {
+      file.forEach((matchedLine) => console.log(matchedLine));
+    });
+    process.exit(0);
+  } else {
+    process.exit(1);
+  }
 } else {
-  console.log("Character is NOT found. Exiting with 1...");
-  process.exit(1);
+  if (matchPattern(inputLine, pattern)) {
+    console.log("Character is found. Exiting with 0...");
+    process.exit(0);
+  } else {
+    console.log("Character is NOT found. Exiting with 1...");
+    process.exit(1);
+  }
 }
 
 function matchPattern(input: string, pattern: string) {
-  const tokens = Tokenizer.tokenize(pattern);
-  console.log(tokens);
+  const tokens: string[] = Tokenizer.tokenize(pattern);
+  // console.log(tokens);
 
-  const [ast] = RegexASTHandler.createRegexSAT(tokens, 0);
-  console.dir(ast, { depth: null, color: true });
+  const [ast]: [RegexAST, number] = RegexASTHandler.createRegexSAT(tokens, 0);
+  // console.dir(ast, { depth: null, color: true });
 
-  let hasAnchorStart = false;
-  let hasAnchorEnd = false;
-
-  if (tokens[0] === "^") {
-    hasAnchorStart = true;
-  }
-  if (tokens.at(-1) === "$") {
-    hasAnchorEnd = true;
-  }
-
-  const maxStart = hasAnchorStart ? 1 : input.length + 1;
-  for (let i = 0; i < maxStart; i++) {
-    if (hasAnchorStart && i !== 0) break;
+  for (let i = 0; i < input.length + 1; i++) {
     if (MatchAST.matchAST(ast, i, input)) {
       return true;
     }
   }
   return false;
+}
+
+function getMatchedLinesFromFile(
+  path: string,
+  pattern: string,
+  withFileName: boolean
+) {
+  const fileContent: Buffer = fs.readFileSync(path);
+  let textContent: string = "";
+  const matchedLines: string[] = [];
+
+  try {
+    textContent = fileContent.toString();
+  } catch (err) {
+    throw new Error("Could not convert to a string");
+  }
+
+  textContent.split("\n").forEach((line) => {
+    if (matchPattern(line, pattern)) {
+      if (withFileName) {
+        matchedLines.push(`${path}:${line}`);
+      } else {
+        matchedLines.push(line);
+      }
+    }
+  });
+
+  return matchedLines;
+}
+
+function getMatchedLinesFromFiles(
+  filePaths: string[],
+  pattern: string
+): string[][] {
+  const matchedLines: string[][] = [];
+
+  filePaths.forEach((filePath) => {
+    const matchedLinesResults = getMatchedLinesFromFile(
+      filePath,
+      pattern,
+      filePaths.length > 1
+    );
+    if (matchedLinesResults.length) {
+      matchedLines.push(matchedLinesResults);
+    }
+  });
+
+  return matchedLines;
 }
